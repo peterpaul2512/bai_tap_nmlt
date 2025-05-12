3e218f370911353218b6a6e96913ac5c0f53401c
peterpaul2512 <dophilau25122002@gmail.com>
2025-05-12 22:01:48 +0700

    add

--- on_thi/2023_2024/cau_4.cpp
+++ on_thi/2023_2024/cau_4.cpp
@@ -2,7 +2,6 @@
 // a. Viết hàm lưu mảng phân số xuống tập tin
 // b. Viết hàm đọc mảng phân số từ tập tin
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
 struct PhanSo {
@@ -10,93 +9,67 @@ struct PhanSo {
     int mau;
 };
 
-void nhapPhanSo(PhanSo &ps);
-
-void xuatPhanSo(PhanSo ps);
-
 void nhapMangPhanSo(PhanSo a[], int &n);
-
-void xuatMangPhanSo(PhanSo a[], int n);
 
 void luuMangPhanSo(PhanSo a[], int n);
 
-int docMangPhanSo(PhanSo a[], int maxSize);
+void docMangPhanSo(PhanSo a[], int &n);
+
+void xuatMangPhanSo(PhanSo a[], int n);
 
 int main() {
-    const int MAX = 100;
-    PhanSo ds1[MAX], ds2[MAX];
+    PhanSo a1[100], a2[100];
     int n1 = 0, n2 = 0;
-
-    // Nhập và lưu
-    nhapMangPhanSo(ds1, n1);
-    luuMangPhanSo(ds1, n1);
-
-    // Đọc lại và in ra
-    n2 = docMangPhanSo(ds2, MAX);
-    cout << "\nMang phan so doc tu file:\n";
-    xuatMangPhanSo(ds2, n2);
-
+    nhapMangPhanSo(a1, n1);
+    luuMangPhanSo(a1, n1);
+    docMangPhanSo(a2, n2);
+    xuatMangPhanSo(a2, n2);
     return 0;
-}
-
-void nhapPhanSo(PhanSo &ps) {
-    cout << "Nhap tu: ";
-    cin >> ps.tu;
-    cout << "Nhap mau: ";
-    cin >> ps.mau;
-}
-
-void xuatPhanSo(PhanSo ps) {
-    cout << ps.tu << "/" << ps.mau << endl;
-}
+};
 
 void nhapMangPhanSo(PhanSo a[], int &n) {
-    cout << "Nhap so luong phan so: ";
+    cout << "Nhap so luong pso: ";
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cout << "Nhap phan so thu " << i + 1 << ":\n";
-        nhapPhanSo(a[i]);
+        cout << "Nhap pso thu " << i + 1 << endl;
+        cout << "Nhap tu: ";
+        cin >> a[i].tu;
+        cout << "Nhap mau: ";
+        cin >> a[i].mau;
     }
-}
-
-// Xuất mảng phân số
-void xuatMangPhanSo(PhanSo a[], int n) {
-    cout << "Mang phan so: " << endl;
-    for (int i = 0; i < n; ++i) {
-        xuatPhanSo(a[i]);
-    }
-    cout << endl;
 }
 
 void luuMangPhanSo(PhanSo a[], int n) {
-    FILE *file = fopen("phanSo.txt", "w");
-    if (file == NULL) {
-        cout << "Khong the mo file de ghi.\n";
-        return;
+    FILE *file = fopen("mang.txt", "w");
+    if (file != NULL) {
+        fprintf(file, "%d\n", n);
+        for (int i = 0; i < n; i++) {
+            fprintf(file, "%d\n", a[i].tu);
+            fprintf(file, "%d\n", a[i].mau);
+        }
+        fclose(file);
+        cout << "Luu vao file thanh cong !" << endl;
+    } else {
+        cout << "Khong the mo file luu." << endl;
     }
-    fprintf(file, "%d\n", n); // Lưu số lượng phần tử
-    for (int i = 0; i < n; i++) {
-        fprintf(file, "%d\n%d\n", a[i].tu, a[i].mau);
-    }
-    fclose(file);
-    cout << "Da luu mang phan so vao file.\n";
 }
 
-int docMangPhanSo(PhanSo a[], int maxSize) {
-    FILE *file = fopen("phanSo.txt", "r");
-    if (file == NULL) {
-        cout << "Khong the mo file de doc.\n";
-        return 0;
+void docMangPhanSo(PhanSo a[], int &n) {
+    FILE *file = fopen("mang.txt", "r");
+    if (file != NULL) {
+        fscanf(file, "%d\n", &n);
+        for (int i = 0; i < n; i++) {
+            fscanf(file, "%d\n%d\n", &a[i].tu, &a[i].mau);
+        }
+        fclose(file);
+        cout << "Doc file thanh cong !" << endl;
+    } else {
+        cout << "Khong the mo file de doc." << endl;
     }
-    int n;
-    fscanf(file, "%d", &n); // Đọc số lượng phần tử
-    if (n > maxSize) {
-        n = maxSize;
-    }
+}
+
+void xuatMangPhanSo(PhanSo a[], int n) {
     for (int i = 0; i < n; i++) {
-        fscanf(file, "%d", &a[i].tu);
-        fscanf(file, "%d", &a[i].mau);
+        cout << a[i].tu << "/" << a[i].mau << endl;
     }
-    fclose(file);
-    return n;
 }
